3c1ec0150b984704dd5866f166cc59a2368ef5ef
Phạm Trọng Thắng <phamtrongthang123@gmail.com>
2025-09-12 12:46:56 -0500

    Update train.sh for SLURM configuration changes

--- Computer Vision and Deep Learning/Deep Learning Fundamentals/Custom loss functions and training loops/Slurm/Multinode without slurm config/train.sh
+++ Computer Vision and Deep Learning/Deep Learning Fundamentals/Custom loss functions and training loops/Slurm/Multinode without slurm config/train.sh
@@ -15,18 +15,19 @@ nvcc --version || echo "nvcc not found"
 
 # Additional PyTorch Lightning specific variables
 export PL_TORCH_DISTRIBUTED_BACKEND="nccl"
-8NODES=${1}
+TOTAL_NODES=${1}
 MASTER_ADDR=${2}
 OFFSET_RANK=${3}
-GPUS_PER_NODE=${4}
-CURRENT_RANK=$((SLURM_PROCID + OFFSET_RANK))
+
+CURRENT_RANK=$((SLURM_NODEID + OFFSET_RANK))
 MASTER_PORT=12340
 
+env | grep SLURM
 source ../.venv/bin/activate
 uv sync  # ensure environment is synced
 uv pip uninstall opencv-python opencv-python-headless
 uv pip install opencv-python-headless
 
-torchrun --nnodes=${TOTAL_NODES} --nproc-per-node=${GPUS_PER_NODE} --node-rank=${CURRENT_RANK} \
+torchrun --nnodes=${TOTAL_NODES} --nproc-per-node=${SLURM_GPUS_ON_NODE} --node-rank=${CURRENT_RANK} \
     --master-addr ${MASTER_ADDR} --master-port=${MASTER_PORT}  \
     train_deepspeed.py
